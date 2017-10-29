d4df1b7c4839bf199300b7e8b82025e69de82bb0
Michael D Pendergrass <mdpendergrass@gmail.com>
2017-10-29 13:51:47 -0400

    clean up parse-number and parse-date
     - remove un-needed multi-methods for parse-number
     - remove parse-date (not used anymore)

--- src/utils/common/parse.clj
+++ src/utils/common/parse.clj
@@ -11,14 +11,8 @@
    :otherwise        (.toBigInteger n)))
 
 (defmulti parse-number type)
-(defmethod parse-number java.lang.Byte          [n] n)
-(defmethod parse-number java.lang.Short         [n] n)
-(defmethod parse-number java.lang.Integer       [n] n)
-(defmethod parse-number java.lang.Long          [n] n)
-(defmethod parse-number java.math.BigInteger    [n] n)
-(defmethod parse-number java.lang.Float         [n] n)
-(defmethod parse-number java.lang.Double        [n] n)
-(defmethod parse-number java.math.BigDecimal    [n] n)
+
+(defmethod parse-number java.lang.Number [n] n)
 
 (defmethod parse-number java.lang.String  [s]
   (try
@@ -29,17 +23,3 @@
     (catch Exception e 0)))
 
 (defmethod parse-number :default [x] 0)
-
-(defn parse-date* [s format]
-  (try
-    (let [sdf (java.text.SimpleDateFormat. format)]
-      (.parse sdf s))
-    (catch java.text.ParseException e)))
-
-(defn parse-date [s formats]
-  (if (> (count formats) 0)
-    (let [format (first formats)]
-      (if-let [date (parse-date* s format)]
-        date
-        (recur s (rest formats))))))
-
