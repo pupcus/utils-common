c70396f26e4a55d71e34d56e78c8cc1ccc7e3095
Michael D Pendergrass <mdpendergrass@gmail.com>
2017-10-29 13:53:07 -0400

    clean up namespace
     - use core fn's rather than custom fn equivalents
     - format long lones
     - make `strong?` configurable

--- src/utils/common/password.clj
+++ src/utils/common/password.clj
@@ -1,18 +1,7 @@
 (ns utils.common.password
   (:refer-clojure :exclude [hash])
-  (:require [clojure.string :as str])
-  (:require [utils.common.hash :as hash])
-  (:require [utils.common.parse :as parse])
-  (:import [org.apache.commons.codec.binary Base64]))
-
-;;
-;; utility functions
-;;
-
-(def rnd (java.util.Random.))
-
-(defn random-element [coll]
-  (nth coll (.nextInt rnd (count coll))))
+  (:require [clojure.string :as str]
+            [utils.common.hash :as hash]))
 
 (def upper-case "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (def lower-case "abcdefghijklmnopqrstuvwxyz")
@@ -22,14 +11,10 @@
 (def characters (map char (concat lower-case numbers punctuation upper-case)))
 
 (defn character []
-  (random-element characters))
+  (rand-nth characters))
 
 (defn random-word [length]
   (apply str (take length (repeatedly character))))
-
-;;
-;; password hashing
-;;
 
 (def digestToAlgorithm {
                         :MD5X :MD5
@@ -48,7 +33,7 @@
                         })
 
 (defn random-digest []
-  (random-element ["md5" "sha" "sha-256" "sha-384" "sha-512"]))
+  (rand-nth ["md5" "sha" "sha-256" "sha-384" "sha-512"]))
 
 (defn iterations [s]
   (inc (reduce + (map #(int %) s))))
@@ -63,9 +48,11 @@
 
 (defn hash [digest pw]
   (let [[digest algorithm] (find-digest-algorithm digest)
-        salt-size (+ 15 (.nextInt rnd 10))
+        salt-size (+ 15 (rand-int 10))
         salt (random-word salt-size)]
-    (String. (Base64/encodeBase64 (.getBytes (str digest "$" salt-size "$" salt "$" (hash/hash algorithm (iterations salt) salt pw)))))))
+    (String.
+     (org.apache.commons.codec.binary.Base64/encodeBase64
+      (.getBytes (str digest "$" salt-size "$" salt "$" (hash/hash algorithm (iterations salt) salt pw)))))))
 
 (def digest-take 4)
 (def salt-size-drop 5)
@@ -79,9 +66,9 @@
   (apply str (drop n s)))
 
 (defn matches? [pw check]
-  (if-let [s (String. (Base64/decodeBase64 (.getBytes check)))]
+  (if-let [s (String. (org.apache.commons.codec.binary.Base64/decodeBase64 (.getBytes check)))]
     (let [digest (str-take digest-take s)
-          size   (parse/parse-number (str-take salt-size-take (str-drop salt-size-drop s)))
+          size   (read-string (str-take salt-size-take (str-drop salt-size-drop s)))
           salt   (str-take size (str-drop salt-drop s))
           hash (str-drop (inc (+ salt-drop size)) s)]
       (= (hash/hash (name ((keyword digest) digestToAlgorithm)) (iterations salt) salt pw) hash))))
@@ -91,27 +78,36 @@
 ;; password strength
 ;;
 
-(def min-length 7)
+(def default-options
+  {:length 8
+   :number-nummeric 2
+   :number-punctuation 1
+   :number-lower-case 1
+   :number-upper-case 1})
 
 (defn contains-char? [c s]
   (some #(= c %) s))
 
-(defn has-lower-case? [s]
-  (> (count (filter #(contains-char? % s) lower-case)) 0))
+(defn has-lower-case? [s number-lower-case]
+  (>= (count (filter #(contains-char? % s) lower-case)) number-lower-case))
 
-(defn has-upper-case? [s]
-  (> (count (filter #(contains-char? % s) upper-case)) 0))
+(defn has-upper-case? [s number-upper-case]
+  (>= (count (filter #(contains-char? % s) upper-case)) number-upper-case))
 
-(defn has-punctuation? [s]
-  (> (count (filter #(contains-char? % s) punctuation)) 0))
+(defn has-punctuation? [s number-punctuation]
+  (>= (count (filter #(contains-char? % s) punctuation)) number-punctuation))
 
-(defn has-number? [s]
-  (> (count (filter #(contains-char? % s) numbers)) 0))
+(defn has-number? [s number-numeric]
+  (>= (count (filter #(contains-char? % s) numbers)) number-numeric))
 
-(defn strong? [s]
-  (and (> (count s) min-length)
-       (has-number? s)
-       (has-punctuation? s)
-       (has-lower-case? s)
-       (has-upper-case? s)))
+(defn strong?
+  ([s]
+   (strong? s default-options))
+  ([s options]
+   (let [{:keys [length number-numeric number-punctuation number-lower-case number-upper-case]} (merge default-options options)]
+     (and (>= (count s) length)
+          (has-number? s number-numeric)
+          (has-punctuation? s number-punctuation)
+          (has-lower-case? s number-lower-case)
+          (has-upper-case? s number-upper-case)))))
 
