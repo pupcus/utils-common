ad4ef6ad0a36e69359c750a61761bb12efe317c6
Michael D Pendergrass <mdpendergrass@gmail.com>
2017-10-29 13:46:57 -0400

    remove dependency on properties file
     - now we pass the keyphrase as an argument

--- src/utils/common/encryption.clj
+++ src/utils/common/encryption.clj
@@ -1,28 +1,22 @@
-(ns utils.common.encryption
-  (:import (javax.crypto.spec DESedeKeySpec))
-  (:import (javax.crypto SecretKey SecretKeyFactory Cipher))
-  (:import (org.apache.commons.codec.binary Base64))
-  (:require [utils.common.properties :as properties]))
-
-(def encryption-properties (properties/load (str (System/getProperty "user.home") "/.properties.d/encryption")))
+(ns utils.common.encryption)
 
 (def create-cipher
-  (memoize (fn [mode]
+  (memoize (fn [mode keyphrase]
              (let [algorithm "DESede"
-                   keySpec (DESedeKeySpec. (.getBytes (:encryption.key encryption-properties)))
-                   keyFactory (SecretKeyFactory/getInstance algorithm)
+                   keySpec (javax.crypto.spec.DESedeKeySpec. (.getBytes ^String keyphrase))
+                   keyFactory (javax.crypto.SecretKeyFactory/getInstance algorithm)
                    key (.generateSecret keyFactory keySpec)]
-               (doto (Cipher/getInstance algorithm)
+               (doto (javax.crypto.Cipher/getInstance algorithm)
                  (.init mode key))))))
 
-(defn protect [text]
-  (->> (.getBytes text)
-       (.doFinal (create-cipher Cipher/ENCRYPT_MODE))
-       (Base64/encodeBase64)
+(defn protect [text keyphrase] {:pre [(string? keyphrase) (>= (count keyphrase) 24)]}
+  (->> (.getBytes ^String text)
+       (.doFinal (create-cipher javax.crypto.Cipher/ENCRYPT_MODE keyphrase))
+       (org.apache.commons.codec.binary.Base64/encodeBase64)
        (String.)))
 
-(defn unprotect [text]
-  (->> (.getBytes text)
-       (Base64/decodeBase64)
-       (.doFinal (create-cipher Cipher/DECRYPT_MODE))
+(defn unprotect [text keyphrase] {:pre [(string? keyphrase) (>= (count keyphrase) 24)]}
+  (->> (.getBytes ^String text)
+       (org.apache.commons.codec.binary.Base64/decodeBase64)
+       (.doFinal (create-cipher javax.crypto.Cipher/DECRYPT_MODE keyphrase))
        (String.)))
